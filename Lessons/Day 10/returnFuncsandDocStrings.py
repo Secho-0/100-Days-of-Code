738c195ca3f4e97071e4be72ef3e8f6add2fcdb3
IsAnEcho <48868031+Secho-0@users.noreply.github.com>
2022-01-18 19:00:43 -0700

    Add day 10, updating art.py with more logo

---
+++ Lessons/Day 10/returnFuncsandDocStrings.py
@@ -0,0 +1,12 @@
+
+def formatName(fname,lname):
+    """
+    Returns someones name in a Capitalized concatenated format
+    """
+    full =  fname.capitalize() + " " + lname.capitalize()
+
+    return full
+
+
+print(formatName('jake','bowe'))
+    
